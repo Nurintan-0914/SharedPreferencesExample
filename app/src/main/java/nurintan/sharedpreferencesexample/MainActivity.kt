e207eaa41f6a2bc63e120d1a5b27c11936fe22e6
Nurintan <nurintann0914@gmail.com>
2024-11-10 14:53:22 +0700

    Add Membagi Data Preferensi Bersama Antar Activity

--- app/src/main/java/nurintan/sharedpreferencesexample/MainActivity.kt
+++ app/src/main/java/nurintan/sharedpreferencesexample/MainActivity.kt
@@ -1,6 +1,8 @@
 package nurintan.sharedpreferencesexample
 
+
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -15,11 +17,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val pref = getPreferences(Context.MODE_PRIVATE)
+        val filename = "$packageName TESTFILE"
+        val pref = getSharedPreferences(filename, Context.MODE_PRIVATE)
+
         binding.btnSave.setOnClickListener {
             val editor = pref.edit()
-            editor.putString("firstName",binding.etFirstName.text.toString())
-            editor.putString("lastName",binding.etLastName.text.toString())
+            editor.putString("firstName",
+                binding.etFirstName.text.toString())
+            editor.putString("lastName",
+                binding.etLastName.text.toString())
             editor.apply()
             Toast.makeText(this, "Saved Data!", Toast.LENGTH_LONG).show()
         }
@@ -31,6 +37,12 @@ class MainActivity : AppCompatActivity() {
             binding.etLastName.setText(lastName)
             binding.tvOutput.text = output
         }
+
+        binding.btnSecondActivity.setOnClickListener{
+            val intent = Intent(this@MainActivity,
+                SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onResume() {
@@ -39,4 +51,8 @@ class MainActivity : AppCompatActivity() {
         binding.etLastName.setText("")
         binding.tvOutput.text = ""
     }
-}
+
+    companion object {
+        const val RPL = "shared_prefs_file"
+    }
+} 
---
+++ app/src/main/java/nurintan/sharedpreferencesexample/SecondActivity.kt
@@ -0,0 +1,31 @@
+package nurintan.sharedpreferencesexample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nurintan.sharedpreferencesexample.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLoad.setOnClickListener {
+            val filename = "$packageName-${MainActivity.RPL}"
+            val pref = getSharedPreferences(filename,
+                Context.MODE_PRIVATE)
+            val firstName = pref.getString("firstName", "")
+            val lastName = pref.getString("lastName", "")
+            binding.tvOutput.text = "$firstName $lastName "
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.tvOutput.text = "Click Button Load Data"
+    }
+}
